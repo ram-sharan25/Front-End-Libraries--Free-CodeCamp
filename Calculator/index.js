140c6c8f517278ac1f1b2180f756ad9af536b485
Ram <https://github.com/rimal-sharan>
2022-06-21 01:20:19 +0545

    Face of calculator Finished

---
+++ Calculator/index.js
@@ -0,0 +1,35 @@
+function App(){
+    return (
+        <div className="container text-center">
+            <div className="grid">
+                        <div className="padButton dis"></div>
+                        <div className="padButton AC">AC</div>
+                        <div className="padButton C">C</div>
+                        <div className="padButton divide">/</div>
+                        <div className="padButton seven">7</div>
+                        <div className="padButton eight">8</div>
+                        <div className="padButton nine">9</div>
+                        <div className="padButton product">*</div>
+                        <div className="padButton four">4</div>
+                        <div className="padButton five">5</div>
+                        <div className="padButton six">6</div>
+                        <div className="padButton minus">-</div>
+                        <div className="padButton one">1</div>
+                        <div className="padButton two">2</div>
+                        <div className="padButton three">3</div>
+                        <div className="padButton decimal">.</div>
+                        <div className="padButton zero">0</div>
+                        <div className="padButton add">+</div>
+                        <div className="padButton equals">=</div>
+                      
+                        
+                       
+                        
+                        
+                      
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"))
