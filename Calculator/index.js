d99ee7205055f3bed247d07c76d2147315761c61
Ram <https://github.com/rimal-sharan>
2022-06-21 19:18:15 +0545

    Calculator FInished

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,34 +1,53 @@
 function App(){
+
+    const [expression,setDisplay] = React.useState("");
+    const [answer,setAnswer]      = React.useState(0);
+   
+
+    const display=(symbol)=>{
+        setDisplay((prev)=>prev+symbol)
+       
+    }
+    const calculate =()=>{
+        setAnswer(eval(expression).toFixed(4));
+        setDisplay(prev=>eval(prev).toFixed(4));
+        
+    }
+   
+    const allClear=()=>{
+        setDisplay("")
+        setAnswer(0);
+    }
     return (
         <div className="container text-center">
             <div className="grid">
-                        <div className="padButton dis"></div>
-                        <div className="padButton AC">AC</div>
-                        <div className="padButton C">C</div>
-                        <div className="padButton divide">/</div>
-                        <div className="padButton seven">7</div>
-                        <div className="padButton eight">8</div>
-                        <div className="padButton nine">9</div>
-                        <div className="padButton product">*</div>
-                        <div className="padButton four">4</div>
-                        <div className="padButton five">5</div>
-                        <div className="padButton six">6</div>
-                        <div className="padButton minus">-</div>
-                        <div className="padButton one">1</div>
-                        <div className="padButton two">2</div>
-                        <div className="padButton three">3</div>
-                        <div className="padButton decimal">.</div>
-                        <div className="padButton zero">0</div>
-                        <div className="padButton add">+</div>
-                        <div className="padButton equals">=</div>
-                      
-                        
-                       
-                        
-                        
-                      
-            </div>
+                
+                <div  className="dis ">
+                    <input  className="input dis" value={expression} type="text" disabled /><br/>
+                </div>
+                <div id="display"  className="total">{answer}</div>
+                <div id="clear" onClick={()=>allClear()} className="padButton AC">AC</div>
+                <div onClick={()=>display("**")} className="padButton POW">^</div>
+                <div onClick={()=>display("/")}id="divide" className="padButton math divide">/</div>
+                <div onClick={()=>display("7")}id="seven" className="padButton number seven">7</div>
+                <div onClick={()=>display("8")}id="eight" className="padButton number eight">8</div>
+                <div onClick={()=>display("9")}id="nine" className="padButton number nine">9</div>
+                <div onClick={()=>display("*")}id="multiply" className="padButton math product">*</div>
+                <div onClick={()=>display("4")}id="four" className="padButton number four">4</div>
+                <div onClick={()=>display("5")}id="five" className="padButton number five">5</div>
+                <div onClick={()=>display("6")}id="six" className="padButton number six">6</div>
+                <div onClick={()=>display("-")}id="subtract" className="padButton  math minus">-</div>
+                <div onClick={()=>display("1")}id="one" className="padButton number one">1</div>
+                <div onClick={()=>display("2")}id="two" className="padButton number two">2</div>
+                <div onClick={()=>display("3")}id="three" className="padButton number three">3</div>
+                <div onClick={()=>display("+")}id="add" className="padButton math add">+</div>
+                <div onClick={()=>display(".")}id="decimal" className="padButton number decimal">.</div>
+                <div onClick={()=>display("0")}id="zero" className="padButton number zero">0</div>
+
+                <div onClick={()=>calculate()} id="equals" className="padButton equals">=</div>                                     
+                                 
         </div>
+    </div>
     )
 }
 
